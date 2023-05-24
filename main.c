5123a1ac551e48bd5a986642a44fef3ee1744027
Mbechi <agnessmminaa@gmail.com>
2023-05-24 10:56:00 -0700

    mbechi

--- main.c
+++ main.c
@@ -1,40 +1,158 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @argc: argc count
- * @argv: argv vector
- *
- * Return: 0 on success, 1 on error
+ * exit_shell - Exit the shell
  */
-
-int main(int argc, char **argv)
+void exit_shell(void)
 {
-	info_t info[] = {INFO_INIT};
-	int fd = STDERR_FILENO;
-
-	if (argc == 2)
-	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-			{
-				exit(126);
-			}
-			else if (errno == ENOENT)
-			{
-				fprintf(stderr, "%s: 0: Can't open %s\n", argv[0], argv[1]);
-				exit(127);
-			}
-			else
-			{
-				perror("open");
-				return (EXIT_FAILURE);
-			}
-		}
-		info->readfd = fd;
-	}
-	return (EXIT_SUCCESS);
+        exit(EXIT_SUCCESS);
 }
 
+/**
+ * print_environment - Print the current environment
+ * @env: The environment variable array
+ */
+void print_environment(char **env)
+{
+        int i = 0;
+
+        while (env[i] != NULL)
+        {
+                printf("%s\n", env[i]);
+                i++;
+        }
+}
+
+/**
+ * env_shell - Implementation of the env built-in command
+ * @env: The environment variable array
+ */
+void env_shell(char **env)
+{
+        print_environment(env);
+}
+
+/**
+ * execute_command - Execute a single command
+ * @command: The command to be executed
+ */
+void execute_command(char *command)
+{
+        pid_t pid;
+        int status;
+
+        pid = fork();
+        if (pid == 0)
+        {
+                char *args[] = {"/bin/sh", "-c", command, NULL};
+                execvp(args[0], args);
+                perror("Error");
+                exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+                perror("Error");
+                exit(EXIT_FAILURE);
+        }
+        else
+        {
+                do
+                {
+                        waitpid(pid, &status, WUNTRACED);
+                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+}
+
+/**
+ * change_directory - Change the current directory
+ * @path: The path to change the directory to
+ */
+void change_directory(char *path)
+{
+        char cwd[256];
+
+        if (path == NULL || strcmp(path, "") == 0)
+        {
+                path = getenv("HOME");
+        }
+
+        if (strcmp(path, "-") == 0)
+        {
+                path = getenv("OLDPWD");
+                printf("%s\n", path);
+        }
+
+        if (chdir(path) != 0)
+        {
+                perror("Error");
+                return;
+        }
+
+        getcwd(cwd, sizeof(cwd));
+        setenv("PWD", cwd, 1);
+}
+
+/**
+ * main - Entry point of the shell program
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ * @env: The environment variable array
+ * Return: EXIT_SUCCESS if successful, EXIT_FAILURE if fail
+ */
+int main(int argc, char **argv, char **env)
+{
+        (void)argc;
+        (void)argv;
+
+        env_shell(env);
+
+        return EXIT_SUCCESS;
+}
+
+/**
+ * execute_command - Execute a single command
+ * @command: The command to be executed
+ */
+void execute_command(char *command)
+{
+        pid_t pid;
+        int status;
+
+        pid = fork();
+        if (pid == 0)
+        {
+                char *args[] = {"/bin/sh", "-c", command, NULL};
+                execvp(args[0], args);
+                perror("Error");
+                exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+                perror("Error");
+                exit(EXIT_FAILURE);
+        }
+        else
+        {
+                do
+                {
+                        waitpid(pid, &status, WUNTRACED);
+                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+}
+
+/**
+ * process_commands - Process multiple commands
+ * @commands: The commands to be processed
+ */
+void process_commands(char *commands)
+{
+        char *token;
+        char *delimiter = ";";
+
+        token = strtok(commands, delimiter);
+        while (token != NULL)
+        {
+                execute_command(token);
+                token = strtok(NULL, delimiter);
+        }
+}
